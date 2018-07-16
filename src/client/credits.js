4549ad3f284f3ddab4a4b1b63c1107d6b5ae1459
asier <asier@scootaloo.local>
2018-07-16 00:55:24 -0400

    Credits now actually pop up a box. It's in the wrong spot, but you can definitely read them.

--- src/client/credits.js
+++ src/client/credits.js
@@ -13,5 +13,23 @@ var credits = new Array()
 
 // print_credits () returns string(html)
 function print_credits() { 
-	return "print_credits NYI"
+	$("#credits-dialog").dialog()
+
+	if ($("#credits-dialog").text().length === 0) {
+		$("#credits-dialog").append("<div id=\"credits-accordion\"></div>")
+
+		for (var x = 0; x < credits.length; x++) {
+			var credit = credits[x]
+			console.log(credit)
+			$("#credits-accordion").append(
+				"<h3>" + credit.name + "</h3>"
+				+ "<div>"
+				+ "<p><a href=\"" + credit.link + "\">Website: " + credit.link + "</a></p>"
+				+ "<p>Version: " + credit.version + "</p>"
+				+ "<p>" + credit.blurb + "</p>"
+				+ "</div>"
+			)
+		}
+		$("#credits-accordion").accordion()
+	}
 }
